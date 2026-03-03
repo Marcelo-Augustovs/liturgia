745d5553fe668bbb414f910cd2c84fc26b24f51d
Marcelo Augusto <marcelo.augustovs99@gmail.com>
2026-03-03 17:47:10 -0300

    pequenos ajustes na pagina inicial

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { MenuPrincipalComponent } from '../../shared/menu-principal/menu-principal.component';
-import { MenuOptionComponent } from '../../shared/menu-option/menu-option.component';
+import { MenuPrincipalComponent } from '../../shared/components/menu-principal/menu-principal.component';
+import { MenuOptionComponent } from '../../shared/components/menu-option/menu-option.component';
 
 @Component({
   selector: 'app-home',
@@ -11,4 +11,30 @@ import { MenuOptionComponent } from '../../shared/menu-option/menu-option.compon
 })
 export class HomeComponent {
 
+cardList = [
+  { title: 'Guia dos Ritos',
+    imageName: 'missal',
+    description: 'Missa, Tempos Liturgicos e Solenidades',
+    linkPage: '/ritos-complementares'},
+
+  { title: 'Capela Musical',
+    imageName: 'capela',
+    description: 'Musicas, Hinos e Orientações',
+    linkPage: '/ritos-complementares'},
+  
+  { title: 'Sacristia',
+    imageName: 'sacristia',
+    description: 'Orientações, Arrumação e Vestes liturgicas',
+    linkPage: '/ritos-complementares'},
+
+  { title: 'Ritos Complementares',
+    imageName: 'ritos2',
+    description: 'Batismo, Matrimonio, Bençãos, Etc... ',
+    linkPage: '/ritos-complementares'},
+
+  { title: 'Atos Devocionais',
+    imageName: 'ritual-de-benção',
+    description: 'descricao',
+    linkPage: '/ritos-complementares'}
+  ]
 }
